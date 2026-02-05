2a51a0cd04c956068a744a29fa6c69e38a086c68
Said CHARFI <said.charfi@Pcp-03.tsn.com>
2026-02-05 11:25:53 +0100

    Version finale avant test email

--- run_app.py
+++ run_app.py
@@ -30,7 +30,7 @@ SUPABASE_URL = st.secrets["SUPABASE"]["URL"]
 SUPABASE_KEY = st.secrets["SUPABASE"]["KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-SUPABASE_SERVICE_KEY = st.secrets["SUPABASE"]["SERVICE_ROLE_KEY"]
+SUPABASE_SERVICE_KEY = st.secrets["SUPABASE"]["KEY"]
 supabase_admin: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 SUPABASE_ONLINE = True
 LOCAL_FILE = "local_records.json"
